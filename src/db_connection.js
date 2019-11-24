1547f99ee1e530602fa04fbe09bd0f57f6ef6651
Marlon Santos <marlonmln.026@gmail.com>
2019-11-24 18:02:58 +0000

    up

--- src/db_connection.js
+++ src/db_connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const con = mysql.createConnection({
     user: 'root',
-    password: 'dbGodinner2019',
-    host: 'db-godinner.cpmvqfvc7pth.sa-east-1.rds.amazonaws.com',
+    password: '',
+    host: ',
     port: 3306,
     database: 'db_posts'
 });
